d5240e3e12982fc636f887f7cf808df2d997eda0
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-07-15 13:38:52 -0700

    reacthookscomponetsabout

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a>
+      <a href="about">About</a>
     </nav>
   );
 }
@@ -11,20 +12,28 @@ function NavBar() {
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+    
     </div>
-  );
+  )
 }
-
 {/* write an <About> component here */}
 
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+  }
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
